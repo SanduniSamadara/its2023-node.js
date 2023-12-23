7f10c9898cd79df7a2f27d6c5a2f995205ccc2b3
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 13:46:34 +0530

    write in files and content is updated in text file

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 
+//////////////////////// read file ////////////////////////////////////
+/**
+ * async - non block
+ * */
 fs.readFile('doc.txt',(err, data) =>{
     if (err){
         console.log(err);
@@ -8,7 +12,16 @@ fs.readFile('doc.txt',(err, data) =>{
 });
 console.log("Called!!"); // print first because read file might take some time . Asynchronous
 
+/**
+ * sync - block
+ * */
 const read = fs.readFileSync('doc.txt');
 console.log("SY: ",read.toString());
 
 console.log("Called2!!");
+
+
+/////////////////////////////// write file ////////////////////////////////////////
+fs.writeFile('doc.txt','Welcome to NodeJS', () =>{
+    console.log("Updated!!")
+})
