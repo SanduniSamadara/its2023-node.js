1ce303d39f317966fa1a1c87ed2eee7c6ccf467a
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 11:22:07 +0530

    Node.js with Git Bash

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 console.log("Welcome to NodeJS");
 const name = "IJSE";
 console.log('Institute: ',name);
+
+// console.log(global);
+global.setTimeout(() => {
+    console.log("Timeout!!");
+},5000)
+
+global.setInterval(() =>{
+    console.log("Interval!!");
+},3000);
