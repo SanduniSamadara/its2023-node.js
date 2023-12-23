f48e84693b8da971f4e35fab21cb4ebdaf299277
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 16:15:14 +0530

    update request handling in express

--- app.js
+++ app.js
@@ -1,49 +1,24 @@
-const http = require('http');
-const fs = require('fs');
+//import
+const express = require('express')
 
-const server = http.createServer((request, response) =>{
-    console.log('req: ',request.url);
-     console.log('res: ',response);
-   /* response.setHeader('Content-Type', 'text/plain');
-    response.write('Hello IJSE');
-    response.end();*/
+// invoke the express
+const app = express();
 
-    response.setHeader('Content-Type', 'text/html');
-   // response.write('<h1>Hello IJSE</h1>');
-    // response.end('<h1>Hello IJSE</h1>');
+// run the server
+app.listen(8081);
 
-
-    let file = 'index.html';
-
-    switch (request.url){
-        case '/':
-            response.statusCode=200;
-            //response.end('\'<h1>Home</h1>\'');
-            file = 'index.html';
-            break;
-        case '/about':
-            response.statusCode=200;
-            //response.end('<h1>About</h1>');
-            file = 'about.html';
-            break;
-        case '/career':
-            response.statusCode=200;
-            file = 'career.html';
-            break;
-        default:
-            response.statusCode=404;
-            file = '404.html';
-            break;
-    }
-    fs.readFile(`./views/${file}`,(err,data) =>{
-        if (err){
-            response.end('<h1>Sorry!Something went wrong</h1>');
-        }else{
-            response.end(data);
-        }
-    })
+app.get('/',(req,resp) =>{
+    resp.send("hello");
 });
 
-server.listen(8081, 'localhost', () =>{
-    console.log("Server Started!!");
+app.get('/about',(req,resp) =>{
+    resp.send("about");
+});
+
+app.get('/career',(req,resp) =>{
+    resp.send("career");
+});
+
+app.use((req, resp) =>{
+    resp.send("404");
 });
