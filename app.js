ff4b856220659536faaf622053d856f59fe1008e
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 13:33:23 +0530

    Asynchronous

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
 
 fs.readFile('doc.txt',(err, data) =>{
+    if (err){
+        console.log(err);
+    }
     console.log(data.toString());
-})
+});
+console.log("Called!!"); // print first because read file might take some time . Asynchronous
