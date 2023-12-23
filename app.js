8b26248fe378cddb26636201ebb552525d97656e
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 12:02:38 +0530

    read a text file and convert byte into string

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
-const os = require('os');
+const fs = require('fs')
 
-/*console.log("Welcome to NodeJS");
-const name = "IJSE";
-console.log('Institute: ',name);*/
-
-// console.log(global);
-/*
-global.setTimeout(() => {
-    console.log("Timeout!!");
-},5000)
-
-global.setInterval(() =>{
-    console.log("Interval!!");
-},3000);
-*/
-
-/**
- * Environmental Variables
- * */
-
-// console.log(__dirname); // absolut path to the directory
-// console.log(__filename); // absolute path to the file
-//
-// console.log("Welcome Nodemon");
-
-/**
- * in-build modules
- * */
-
-console.log(os.platform()); // Returns a string identifying the operating system platform for which the Node.js binary was compiled. platform of the os
-console.log(os.version()); // return the version
-console.log(os.release()); // return the released v
-console.log(os.homedir()); // Returns the string path of the current user's home directory.
+fs.readFile('doc.txt',(err, data) =>{
+    console.log(data.toString());
+})
