1e47e12df628e82e83217348137f906268b8d6e3
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 11:54:47 +0530

    in-build modules

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-console.log("Welcome to NodeJS");
+const os = require('os');
+
+/*console.log("Welcome to NodeJS");
 const name = "IJSE";
-console.log('Institute: ',name);
+console.log('Institute: ',name);*/
 
 // console.log(global);
 /*
@@ -17,5 +19,16 @@ global.setInterval(() =>{
  * Environmental Variables
  * */
 
-console.log(__dirname); // absolut path to the directory
-console.log(__filename); // absolute path to the file
+// console.log(__dirname); // absolut path to the directory
+// console.log(__filename); // absolute path to the file
+//
+// console.log("Welcome Nodemon");
+
+/**
+ * in-build modules
+ * */
+
+console.log(os.platform()); // Returns a string identifying the operating system platform for which the Node.js binary was compiled. platform of the os
+console.log(os.version()); // return the version
+console.log(os.release()); // return the released v
+console.log(os.homedir()); // Returns the string path of the current user's home directory.
