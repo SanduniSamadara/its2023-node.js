94889aad658267fe9d0276666fcdfc44f8cb0301
SanduniSamadara <121673809+SanduniSamadara@users.noreply.github.com>
2023-12-23 15:57:57 +0530

    update requests handling

--- app.js
+++ app.js
@@ -1,71 +1,49 @@
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
 
-//////////////////////// read file ////////////////////////////////////
-/**
- * async - non block
- * */
-/*
-fs.readFile('doc.txt',(err, data) =>{
-    if (err){
-        console.log(err);
+const server = http.createServer((request, response) =>{
+    console.log('req: ',request.url);
+     console.log('res: ',response);
+   /* response.setHeader('Content-Type', 'text/plain');
+    response.write('Hello IJSE');
+    response.end();*/
+
+    response.setHeader('Content-Type', 'text/html');
+   // response.write('<h1>Hello IJSE</h1>');
+    // response.end('<h1>Hello IJSE</h1>');
+
+
+    let file = 'index.html';
+
+    switch (request.url){
+        case '/':
+            response.statusCode=200;
+            //response.end('\'<h1>Home</h1>\'');
+            file = 'index.html';
+            break;
+        case '/about':
+            response.statusCode=200;
+            //response.end('<h1>About</h1>');
+            file = 'about.html';
+            break;
+        case '/career':
+            response.statusCode=200;
+            file = 'career.html';
+            break;
+        default:
+            response.statusCode=404;
+            file = '404.html';
+            break;
     }
-    console.log(data.toString());
-});
-console.log("Called!!"); // print first because read file might take some time . Asynchronous
-
-/!**
- * sync - block
- * *!/
-const read = fs.readFileSync('doc.txt');
-console.log("SY: ",read.toString());
-
-console.log("Called2!!");
-
-
-/////////////////////////////// write file ////////////////////////////////////////
-fs.writeFile('doc.txt','Welcome to NodeJS', () =>{
-    console.log("Updated!!")
-})
-
-
-/////////////////////////////// create directory ////////////////////////////////////////
-fs.mkdir('./doc1',(err =>{
-    console.log("Created!!");
-}))
-
-
-/////////////////////////////// remove directory ////////////////////////////////////////
-fs.rmSync('./doc1',{recursive: true,force: true},() =>{});
-
-
-/////////////////////////////// remove file ////////////////////////////////////////
-//async
-fs.unlink('.doc2.txt',() =>{
-    console.log("Removed the file!!")
-});
-*/
-
-
-/////////////////////////////// add new content ////////////////////////////////////////
-fs.appendFile('doc3.txt','Welcome', (err) =>{
-    if (err){
-        console.log(err);
-    }else {
-        console.log("New File Created!!");
-    }
-});
-
-
-
-/////////////////////////////// add new content in new line ////////////////////////////////////////
-if (fs.existsSync('doc3.txt')) {
-    fs.appendFile('doc3.txt', '\nIJSE', (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("New File Created!!");
+    fs.readFile(`./views/${file}`,(err,data) =>{
+        if (err){
+            response.end('<h1>Sorry!Something went wrong</h1>');
+        }else{
+            response.end(data);
         }
-    });
-}else{
-    // code
-    }
+    })
+});
+
+server.listen(8081, 'localhost', () =>{
+    console.log("Server Started!!");
+});
